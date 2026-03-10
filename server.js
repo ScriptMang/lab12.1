4484fb6f40528c98ac45f7e9953746b814950aa8
scriptMang <aperalta686@gmail.com>
2026-03-09 21:26:47 -0400

    Add get request for index-page

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import path from 'path'
 
-
-const app = express()
+const app = express();
 const port = 3000;
 
+const __dirname = import.meta.dirname;
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+
 app.listen(port, ()=>{
-    console.log(`Listening on server port: ${port}`)
+    console.log(`Listening on server port: ${port}`);
 })
